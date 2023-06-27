c04be2c1cc3ce4ec653b760c380c396f0751efef
Saqib Ahmad <you@example.com>
2023-06-27 12:15:35 +0600

    done

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,4 +1,4 @@
-const tools = [
+let tools = [
     { id: 1, tool: "Hammer" },
     { id: 2, tool: "Hammer2" },
     { id: 3, tool: "Hammer3" },
@@ -18,4 +18,18 @@ module.exports.getATool = (req, res) => {
     const id = req.params.id;
     const selected = tools.find(tool => tool.id == id)
     res.send(selected)
+}
+
+module.exports.updateATool = (req, res) => {
+    const { id } = req.params;
+    const updatedOne = tools.find(tool => tool.id == id);
+    updatedOne.id = Number(id);
+    updatedOne.tool = req.body?.tool;
+    res.send(tools);
+}
+
+module.exports.deleteATool = (req, res) => {
+    const { id } = req.params;
+    tools = tools.filter(tool => tool.id != id);
+    res.send(tools);
 }
--- routes/v1/tools.route.js
+++ routes/v1/tools.route.js
@@ -3,22 +3,15 @@ const toolsController = require('../../controllers/tools.controller');
 const { viewCounter, limiter } = require('../../middleWare/middleWare');
 
 const router = express.Router();
-/* 
-router.get('/', (req, res) => {
-    res.send("All tools found");
-})
-
-router.post('/', (req, res) => {
-    res.send("toll posted");
-})
- */
 
 router.route('/')
     .get(toolsController.getAllTools)
     .post(toolsController.postATool)
 
 router.route('/:id')
-    .get(limiter,viewCounter, toolsController.getATool)
+    .get(limiter, viewCounter, toolsController.getATool)
+    .patch(toolsController.updateATool)
+    .delete(toolsController.deleteATool)
 
 
 module.exports = router;
